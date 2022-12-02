152120e1cf495d6fbf166fec8371f4b141bfbed8
Maxim Khromov <max.khromov.ig@gmail.com>
2022-12-02 16:59:16 +0100

    project structure created

---
+++ resources/js/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./Coffee-shop/App";
+ 
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>HOME</title>
+</head>
+<body>
+  
+    <div id="root"></div>
+    @viteReactRefresh
+    @vite('resources/js/index.jsx')
+    {{-- <script type="text/javascript">      
+    window.csrf_token = '{{ csrf_token() }}'
+    </script> --}}
+</body>
+</html>
