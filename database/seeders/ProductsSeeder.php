7147cd12b13227697c7e8d6e9802c12d0b04449d
Maxim Khromov <max.khromov.ig@gmail.com>
2022-12-06 17:20:00 +0100

    seeder

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -15,11 +15,105 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
+        $coffees = [
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1, 
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+                [
+                    'name' => 'Armondo',
+                    'category_id' => 1,
+                    'flavor' => 'Chocolate',
+                    'discription' => 'Unique chocolate coffee from Africa',
+                    'manufacturer' => 'Africa',
+                    'price' => 23,
+                    'quantity' => 150
+                ],
+            ];
+
+
         $product = new Product();
-        $product->name = "test";
-        $product->category_id = 1;
-        $product->price = 100;
-        $product->quantity = 10;
-        $product->save();
+
+        foreach ($coffees as $coffee) {
+            
+            $product->name = $coffee->name;
+            $product->category_id = $coffee->category_id;
+            $product->flavor = $coffee->flavor;
+            $product->discription = $coffee->discription;
+            $product->manufacturer = $coffee->manufacturer;
+            $product->price = $coffee->price;
+            $product->quantity = $coffee->quantity;
+            $product->save();
+        }
+
+
+        // $product = new Product();
+        // $product->name = 'Armondo';
+        // $product->category_id = 1;
+        // $product->flavor = 'Chocolate';
+        // $product->discription = 'Unique chocolate coffee from Africa';
+        // $product->manufacturer = 'Africa';
+        // $product->price = 23;
+        // $product->quantity = 150;
+        // $product->save();
     }
 }
